090f0ecef4b7cb96f66777bff11fb07bb1c069c2
jsplay <jsplay@users.noreply.github.com>
2016-08-02 19:01:26 +0400

    Add files via upload

---
+++ tscrambler.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jay Dee on 02/08/16.
+ */
+
+var tags = ["b", "i", "sup", "sub", "ins", "u", "strike", "tt", "big", "small"];
+
+function scramble(inputString) {
+    var out = "";
+    for (var i=0; i < inputString.length; i++) {
+        r =  Math.floor(Math.random() * tags.length);
+        out += "<" + tags[r] + ">" + inputString[i] + "</" + tags[r] + ">";
+    }
+    return out;
+}
